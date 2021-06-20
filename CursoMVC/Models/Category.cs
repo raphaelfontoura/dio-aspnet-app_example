0aae7a171f0a14e2a71536555b47d65340781920
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-06-19 22:23:37 -0300

    Add DataAnnotations and modified views to show description text

--- CursoMVC/Models/Category.cs
+++ CursoMVC/Models/Category.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CursoMVC.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
         public string Description { get; set; }
         public List<Product> Products { get; set; }
     }
--- CursoMVC/Models/Product.cs
+++ CursoMVC/Models/Product.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CursoMVC.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        // [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "The Description field is mandatory")]
         public string Description { get; set; }
+        [Range(1,10)]
         public int Quantity { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
